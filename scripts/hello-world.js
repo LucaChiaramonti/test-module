2f5cde6afd4a698dd7da262e873eefa885df316b
LucaChiaramonti <spiffychiaramonti@gmail.com>
2021-09-15 22:44:53 +0200

    added ability score check

--- scripts/hello-world.js
+++ scripts/hello-world.js
@@ -1,46 +1,46 @@
-console.log("Hello World! This code runs immediately when the file is loaded.");
-
-Hooks.on("chatMessage", (dialog, $element, targets) => {
-  if($element=="Hello") {
-    ChatMessage.create({
-      "content":" HELLO WORLD",
-      "speaker": {
-          "alias": "Spiffy"
-      }
-    }).then()
-  }
-});
-
-Hooks.on("updateDocument", (document, change, options, userId) => {
-//    console.log(document);
-//   console.log(change);
-//    console.log(options);
-//    console.log(userId);
-
-    ChatMessage.create({
-      "content":"Update!",
-      "speaker": {
-          "alias": "Spiffy"
-      }
-    }).then()
-});
-
-const myHookId = Hooks.on('updateActor', this.onUpdateActor.bind(this));
-
-//function declaration (same context) the argument are declared in the same order as the original call
-function onUpdateActor(actor, data, options, userId){
+Hooks.on('updateActor', async (actor, data, options, userId) => {
   if(options.diff == true) {
+    
     var name = actor.data.name
+    console.log(userId);
+    console.log(data.data.abilities);
+    console.log(actor);
+    var result = checkAbilities(data.data.abilities);
+    var name = actor.data.name;
     ChatMessage.create({
-      "content":"Updated: " +name + " \n UserId: " + userId,
+      "content":"Character " + name + " has been updated! " + result,
       "speaker": {
           "alias": "Spiffy"
       }
     }).then()
   }
-    console.log(actor);
-    console.log(data);
-    console.log(options);
-    console.log(userId);
-}
+})
 
+function checkAbilities(data) {
+  var cha = data.cha;
+  var con = data.con;
+  var dex = data.dex;
+  var int = data.int;
+  var str = data.str;
+  var wis = data.wis;
+  var result = "";
+  if( (typeof cha.tempvalue !== 'undefined') && (typeof cha.total !== 'undefined')) {
+    result+= "Charisma changed from " + cha.tempvalue[1] + " to " + cha.tempvalue[0];
+  }
+  if( (typeof con.tempvalue !== 'undefined') && (typeof con.total !== 'undefined')) {
+    result+= "Constitution changed from " + con.tempvalue[1] + " to " + con.tempvalue[0];
+  }
+  if( (typeof dex.tempvalue !== 'undefined') && (typeof dex.total !== 'undefined')) {
+    result+= "Dexterety changed from " + dex.tempvalue[1] + " to " + dex.tempvalue[0];
+  }
+  if( (typeof int.tempvalue !== 'undefined') && (typeof int.total !== 'undefined')) {
+    result+= "Intelligence changed from " + int.tempvalue[1] + " to " + int.tempvalue[0];
+  }
+  if( (typeof str.tempvalue !== 'undefined') && (typeof str.total !== 'undefined')) {
+    result+= "Strenght changed from " + str.tempvalue[1] + " to " + str.tempvalue[0];
+  }
+  if( (typeof wis.tempvalue !== 'undefined') && (typeof wis.total !== 'undefined')) {
+    result+= "Wisdom changed from " + wis.tempvalue[1] + " to " + wis.tempvalue[0];
+  }
+  return result;
+}
