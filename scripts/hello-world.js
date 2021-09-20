647723ebe87d4acc2923d541bfd9c65816bfa976
LucaChiaramonti <spiffychiaramonti@gmail.com>
2021-09-20 21:25:45 +0200

    added playerName in the message and added whisper to gm

--- scripts/hello-world.js
+++ scripts/hello-world.js
@@ -1,18 +1,25 @@
+
+
 Hooks.on('updateActor', async (actor, data, options, userId) => {
   if(options.diff == true) {
-    
-    var name = actor.data.name
-    console.log(userId);
-    console.log(data.data.abilities);
-    console.log(actor);
+    var users = Array.from(game.collections.get("User").entries());
+
+    var name = actor.data.name;
+
     var result = checkAbilities(data.data.abilities);
     var name = actor.data.name;
-    ChatMessage.create({
-      "content":"Character " + name + " has been updated! " + result,
-      "speaker": {
-          "alias": "Spiffy"
+    var userName = "";
+    users.forEach(function(user) {
+      if(user[0] === userId) {
+        userName = user[1].data.name;
       }
-    }).then()
+    });
+    var content = "Character " + name + " has been updated! " + result + " by " + userName;
+    var whisper = game.users.entities.filter(u => u.isGM).map(u => u._id);
+    var speaker = {"alias": "Speaker"};
+
+    sendMessage(content, whisper, speaker);
+    
   }
 })
 
@@ -24,23 +31,31 @@ function checkAbilities(data) {
   var str = data.str;
   var wis = data.wis;
   var result = "";
-  if( (typeof cha.tempvalue !== 'undefined') && (typeof cha.total !== 'undefined')) {
+  if( (typeof cha !== 'undefined') && (typeof cha.total !== 'undefined')) {
     result+= "Charisma changed from " + cha.tempvalue[1] + " to " + cha.tempvalue[0];
   }
-  if( (typeof con.tempvalue !== 'undefined') && (typeof con.total !== 'undefined')) {
+  if( (typeof con !== 'undefined') && (typeof con.total !== 'undefined')) {
     result+= "Constitution changed from " + con.tempvalue[1] + " to " + con.tempvalue[0];
   }
-  if( (typeof dex.tempvalue !== 'undefined') && (typeof dex.total !== 'undefined')) {
+  if( (typeof dex !== 'undefined') && (typeof dex.total !== 'undefined')) {
     result+= "Dexterety changed from " + dex.tempvalue[1] + " to " + dex.tempvalue[0];
   }
-  if( (typeof int.tempvalue !== 'undefined') && (typeof int.total !== 'undefined')) {
+  if( (typeof int !== 'undefined') && (typeof int.total !== 'undefined')) {
     result+= "Intelligence changed from " + int.tempvalue[1] + " to " + int.tempvalue[0];
   }
-  if( (typeof str.tempvalue !== 'undefined') && (typeof str.total !== 'undefined')) {
+  if( (typeof str !== 'undefined') && (typeof str.total !== 'undefined')) {
     result+= "Strenght changed from " + str.tempvalue[1] + " to " + str.tempvalue[0];
   }
-  if( (typeof wis.tempvalue !== 'undefined') && (typeof wis.total !== 'undefined')) {
+  if( (typeof wis !== 'undefined') && (typeof wis.total !== 'undefined')) {
     result+= "Wisdom changed from " + wis.tempvalue[1] + " to " + wis.tempvalue[0];
   }
   return result;
+}
+
+function sendMessage(content, whisper, speaker) {
+  ChatMessage.create({
+    "content": content,
+    "whisper": whisper,
+    "speaker": speaker
+  }).then()
 }
